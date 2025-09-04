88fe43367be0ca5b328a23ba2520ff4accdf7b4d
KWAshen <ashen.maliaha@gmail.com>
2025-09-04 21:26:39 +0200

    fine secondo Milestone

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const articoli = [
   "Costume",
   "Ombrellone",
@@ -7,19 +8,39 @@ const articoli = [
 ];
 
 function App() {
+  const [newarticle, setnewarticle] = useState("")
+  const [article, setarticle] = useState(articoli);
+  
+  const handlesubmit = (a) => {
+    a.preventdefaoult();
+   
+    const newarticle = [...article,newarticle];
+    setarticle(newarticle);
+  };
   return (
     <>
-     <div className="container mt-5">
+     <div className="container my-5">
       <div className="row">
-        <div className="col-12">
+        <div className="col-12 gy-3">
           <h1>Lista articoli</h1>
         </div>
         <div className="col-12">
           <ul className="lista-unstyled ms-0">
-            {articoli.map((article, index) =>{
+            {articles.map((article, index) =>{
               return <li key={index}>{article}</li>
             })}
           </ul>
+        </div>
+        <div className="col-12">
+          <form onSubmit={handlesubmit} className="mt-3">
+            <div className="d-flex">
+             <input type="text" 
+             className="form-control me-2" placeholder="aggiungi articolo"
+             value={newarticole}
+             onChange={(a) => setnewarticole(a.target.value)}/>
+             <button className="btn btn-success">aggiungi</button>
+            </div>
+          </form>
         </div>
       </div>
      </div>
