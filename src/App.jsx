ceec1388a78674c97dcce50fe30d9a6a212fffc3
KWAshen <ashen.maliaha@gmail.com>
2025-09-04 20:34:20 +0200

    fine Milestone

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-
+const articoli = [
+  "Costume",
+  "Ombrellone",
+  "Crema solare",
+  "Ciabatte",
+  "Telo Mare",
+];
 
 function App() {
   return (
     <>
-     <h1>Hello World</h1>
+     <div className="container mt-5">
+      <div className="row">
+        <div className="col-12">
+          <h1>Lista articoli</h1>
+        </div>
+        <div className="col-12">
+          <ul className="lista-unstyled ms-0">
+            {articoli.map((article, index) =>{
+              return <li key={index}>{article}</li>
+            })}
+          </ul>
+        </div>
+      </div>
+     </div>
     </>
   )
 }
